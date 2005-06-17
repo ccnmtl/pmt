c745a0f8eb27e00af0a70d3db0c2287a92b334ac
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2005-06-17 17:37:40 +0000

    moved add_item.pl's functionality into PMT::Control
    git-svn-id: http://svn.ccnmtl.columbia.edu/pmt/trunk@82 1f418930-7ff8-0310-b8ad-c653122473bc

--- PMT/Control.pm
+++ PMT/Control.pm
@@ -35,6 +35,7 @@ sub setup {
         'my_groups'              => 'my_groups',
         'project_search_form'    => 'project_search_form',
         'add_item_form'          => 'add_item_form',
+        'add_item'               => 'add_item',
         'global_reports'         => 'global_reports',
         'add_trackers_form'      => 'add_trackers_form',
         'add_trackers'           => 'add_trackers',
@@ -364,6 +365,129 @@ sub add_item_form {
     $template->param(items_mode => 1);
     $template->param(page_title => "Add $type");
     return $template->output();
+}
+
+sub add_item {
+    my $self = shift;
+    my $user = $self->{user};
+    my $cgi = $self->query();
+    my $pmt = $self->{pmt};
+
+    my $username = $user->{username};
+
+    my $type         = $cgi->param('type') || throw Error::NO_TYPE "type is necessary";
+    my $pid          = $cgi->param('pid') || throw Error::NO_PID "no project specified";
+    my $mid          = $cgi->param('mid') || "";
+    if ($mid eq "") {
+	my $project = PMT::Project->retrieve($pid);
+	$mid = $project->upcoming_milestone();
+    }
+    my $title = escape($cgi->param('title')) || "no title";
+    
+    #Min's changes to implement multiple assignees to an action item
+    #my $assigned_to  = $cgi->param('assigned_to') || "";
+    my @assigned_to  = $cgi->param('assigned_to');
+    my $owner        = $cgi->param('owner') || $username;
+    my $priority     = $cgi->param('priority') || "";
+    my $year         = $cgi->param('year') || "";
+    my $month        = $cgi->param('month') || "";
+    my $day          = $cgi->param('day') || "";
+    my $url          = escape($cgi->param('url')) || "";
+    my $description  = $cgi->param('description') || "";
+    my $new_keywords = $cgi->param('new_keywords') || "";
+    my @keywords     = $cgi->param('keywords');
+    my @dependencies = $cgi->param('depends');
+    my @clients      = $cgi->param('clients');
+    my $completed    = $cgi->param('completed') || "";
+
+
+    my $target_date = $cgi->param('target_date') || "";
+    my $estimated_time = $cgi->param('estimated_time') || "";
+
+    if($target_date =~ /(\d{4}-\d{2}-\d{2})/) {
+	$target_date = $1;
+    } else {
+	my $milestone = PMT::Milestone->retrieve($mid);
+	$target_date = $milestone->target_date;
+    }
+    if ($estimated_time =~ /^(\d+)$/) {
+        $estimated_time .= "h";
+    }
+    if ($estimated_time eq "") {
+        $estimated_time = "0h";
+    }
+    push @keywords, split /\n/, $new_keywords;
+    @keywords = map {escape($_);} @keywords;
+
+    my @new_keywords;
+    foreach my $k (@keywords) {
+	push @new_keywords, $k unless $k eq "";
+    }
+
+    my @new_dependencies;
+    foreach my $d (@dependencies) {
+	push @new_dependencies, $d unless $d eq "";
+    }
+    my @new_clients;
+    foreach my $client (@clients) {
+	push @new_clients, $client unless $client eq "";
+    }
+
+    #Min's changes to implement multiple assignees to an action item
+    # loop through each assignee, creating a new action item for
+    # each assignee
+    #foreach my $assignee (@assigned_to) {
+
+        if($type eq "tracker") {
+    	    my $resolve_time = $cgi->param('time') || "1 hour";
+	    if($resolve_time =~ /^(\d+)$/) {
+	        # default to hours if now unit is specified
+	        $resolve_time = "$1"."h";
+	    }
+	    $pmt->add_tracker(pid => $pid,
+	    		      mid => $mid,
+			      title => $title,
+			      'time' => $resolve_time,
+			      target_date => $target_date,
+			      owner => $username,
+			      completed => $completed,
+			      clients => \@new_clients);
+        } elsif ($type eq "todo") {
+	    $pmt->add_todo(pid => $pid,
+		           mid => $mid,
+		           title => $title,
+		           target_date => $target_date,
+		           owner => $username);
+        } else {
+    #Min's changes to implement multiple assignees to an action item
+    # loop through each assignee, creating a new action item for
+    # each assignee
+            foreach my $assignee (@assigned_to) {
+
+               my %item = (type         => $type,
+	                pid          => $pid,
+		        mid          => $mid,
+		        title        => $title,
+		        assigned_to  => $assignee,
+		        owner        => $owner,
+		        priority     => $priority,
+		        target_date  => $target_date,
+		        url          => $url,
+		        description  => $description,
+		        keywords     => \@new_keywords,
+		        dependencies => \@new_dependencies,
+		        clients      => \@new_clients,
+		        estimated_time => $estimated_time);
+	       $pmt->add_item(\%item);
+	    }
+	$type =~ s/\s/%20/g;
+      }
+    # put the user back at the add item for for the same type/project
+    # so they can conveniently add multiple items
+    $self->header_type('redirect');
+    $self->header_props(-url => "home.pl?mode=add_item_form;type=$type;pid=$pid");
+    return "redirecting back to add item form";
+
 }
 
 sub add_trackers_form {
