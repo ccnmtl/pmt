31d27ff1d27b18258dfc2b2e8ce73cf0817bde3c
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2005-06-17 17:25:14 +0000

    removed unused print_form()
    git-svn-id: http://svn.ccnmtl.columbia.edu/pmt/trunk@81 1f418930-7ff8-0310-b8ad-c653122473bc

--- add_item.pl
+++ add_item.pl
@@ -142,37 +142,3 @@ if($@) {
     }
 }
 
-
-
-
-sub print_form {
-    my $pid = shift;
-    my $type = shift;
-    my $username = shift;
-    my $cgi = shift;
-    my $pmt = shift;
-    my $user = new PMT::User($username);
-
-
-    
-    my $template = template("add_item.tmpl");
-
-
-    $template->param($user->menu());
-    $template->param($pmt->add_item_form($pid,$type,$username));
-
-    use Date::Calc qw/Week_of_Year Monday_of_Week Add_Delta_Days/;
-    my ($sec,$min,$hour,$mday,$mon,
-	$year,$wday,$yday,$isdst) = localtime(time); 
-    $year += 1900;
-    $mon += 1;
-    my ($mon_year,$mon_month,$mon_day) = Monday_of_Week(Week_of_Year($year,$mon,$mday));
-    # backdated stuff goes in on sundays
-    my ($p_year,$p_month,$p_day) = Add_Delta_Days($mon_year,$mon_month,$mon_day,-1);
-    my ($pp_year,$pp_month,$pp_day) = Add_Delta_Days($mon_year,$mon_month,$mon_day,-8);
-
-    $template->param(p_week => "$p_year-$p_month-$p_day",
-		     pp_week => "$pp_year-$pp_month-$pp_day");
-
-    print $cgi->header(-charset => 'utf-8'), $template->output();
-}
