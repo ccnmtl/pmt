7c02a7272ac4d632731de75a2ba40e0647f1bc87
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2005-06-22 21:16:01 +0000

    moved client_search.pl into runmodes
    git-svn-id: http://svn.ccnmtl.columbia.edu/pmt/trunk@137 1f418930-7ff8-0310-b8ad-c653122473bc

--- PMT/Control.pm
+++ PMT/Control.pm
@@ -93,6 +93,8 @@ sub setup {
         'project_search'         => 'project_search',
         'edit_client'            => 'edit_client',
         'edit_client_form'       => 'edit_client_form',
+        'client_search'          => 'client_search',
+        'client_search_form'     => 'client_search_form',
     );
     my $pmt = new PMT();
     my $q = $self->query();
@@ -2376,6 +2378,95 @@ sub edit_client_form {
                      contacts_select => $client->contacts_select(),
                      schools_select => $client->schools_select(),
                      departments_select => $client->all_departments_select());
+    $template->param(clients_mode => 1);
+    return $template->output();
+}
+
+sub client_search_form {
+    my $self = shift;
+    my $cgi = $self->query();
+    my $pmt = $self->{pmt};
+    my $template = $self->template("client_search_form.tmpl");
+    my $sql = qq{select distinct school,upper(school) as uschool from clients order by upper(school);};
+    $template->param(schools => $pmt->s($sql,[],['school','uschool']));
+    $sql = qq{select distinct department,upper(department) as udep from clients order by upper(department);};
+    $template->param(departments => $pmt->s($sql,[],['department','udep']));
+    $sql = qq{select distinct c.contact,u.fullname,upper(u.fullname) from clients c, users u 
+		  where c.contact = u.username order by upper(u.fullname) ASC;};
+    $template->param(contacts => $pmt->s($sql,[],['contact_username','contact_fullname']));
+    $sql = qq{select min(registration_date) from clients;};
+    $template->param(start_date => $pmt->ss($sql,[],['reg'])->{reg});
+    $sql = qq{select current_date;};
+    $template->param(end_date => $pmt->ss($sql,[],['date'])->{date});
+
+    $template->param(page_title => "client search");
+    $template->param(clients_mode => 1);
+    return $template->output();
+}
+
+sub client_search {
+    my $self = shift;
+    my $cgi = $self->query();
+    my $pmt = $self->{pmt};
+
+    my $status = $cgi->param('status') || "%";
+    my $department = $cgi->param('department') || "%";
+    my $school = $cgi->param('school') || "%";
+    my $start_date = $cgi->param('start_date') || "1900-01-01";
+    my $end_date = $cgi->param('end_date') || "2500-01-01";
+    my $project = $cgi->param('project') || "%";
+    my $limit = $cgi->param('limit') || 100;
+    my $offset = $cgi->param('offset') || 0;
+    my $contact = $cgi->param('contact') || "%";
+
+    my $q = $cgi->param('q') || "%";
+    $limit =~ s/\D//;
+
+    my $template = $self->template("client_search_results.tmpl");
+    $template->param(results => [map {
+	$_->{inactive} = $_->{status} eq "inactive";
+	$_;
+    } @{$pmt->client_search(
+			    query => $q,
+			    status => $status,
+			    department => $department,
+			    school => $school,
+			    start_date => $start_date,
+			    end_date => $end_date,
+			    project => $project,
+			    limit => $limit,
+			    offset => $offset,
+			    contact => $contact,
+			    )}]);
+    my $results_count = $pmt->client_search_count(
+						  query => $q,
+						  status => $status,
+						  department => $department,
+						  school => $school,
+						  start_date => $start_date,
+						  end_date => $end_date,
+						  project => $project,
+						  contact => $contact,
+						  );
+    $template->param(results_count => $results_count);
+    if ($results_count > ($offset + $limit)) {
+	$template->param('next' => 1);
+	$template->param('next_offset' => $offset + $limit);
+    }
+    if ($offset > 0) {
+	$template->param('prev' => 1);
+	$template->param('prev_offset' => $offset - $limit);
+    }
+    $template->param(limit => $limit,
+		     q => $q,
+		     status => $status,
+		     department => $department,
+		     school => $school,
+		     start_date => $start_date,
+		     end_date => $end_date,
+		     project => $project,
+		     contact => $contact);
+    $template->param(page_title => "client search");
     $template->param(clients_mode => 1);
     return $template->output();
 }
