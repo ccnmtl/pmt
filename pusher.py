3d484deaae084fd3fb5d78bb80007a418dee3fad
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2005-07-28 18:43:09 +0000

    added unit tests step to pusher script
    git-svn-id: http://svn.ccnmtl.columbia.edu/pmt/trunk@455 1f418930-7ff8-0310-b8ad-c653122473bc

--- pusher.py
+++ pusher.py
@@ -1,3 +1,8 @@
+def run_unit_tests(pusher):
+    codir = pusher.checkout_dir()
+    (out,err) = pusher.execute("pushd %s && ./test.pl && popd" % codir)
+    return ("All tests successful." in out, out, err)
+
 def post_rsync(pusher):
     """ we need to kick apache1 to get it to pick up the changes """
     (out,err) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","sudo","/etc/init.d/apache","restart"])
