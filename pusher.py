360a865d966761ba8ee46871805659e0b06518fb
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2006-12-07 21:30:25 +0000

    no need to kick apache now that the PMT is CGI on production
    git-svn-id: http://svn.ccnmtl.columbia.edu/pmt/trunk@5663 1f418930-7ff8-0310-b8ad-c653122473bc

--- pusher.py
+++ pusher.py
@@ -5,5 +5,6 @@ def run_unit_tests(pusher):
 
 def post_rsync(pusher):
     """ we need to kick apache1 to get it to pick up the changes """
-    (out,err) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","sudo","/etc/init.d/apache2","restart"])
+    # not anymore, actually. PMT's running as CGI now on frink.
+    #    (out,err) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","sudo","/etc/init.d/apache2","restart"])
     return (True,out,err)
